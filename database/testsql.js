f8f0d63780d5abcf878be6b744f1f88cafc45064
Toby Quality <topias.laatu@gmail.com>
2023-12-19 08:42:36 +0200

    testsql.js:Copy-paste content from another project, change later

--- database/testsql.js
+++ database/testsql.js
@@ -1,1 +1,26 @@
 // UNDER CONSTRUCTION
+const connection = require("./mysql_connector.js");
+
+//perform crud operations
+const main = async () => {
+  try {
+    await connection.connect().then(() => console.log("Connected to database"));
+    //const findAllResult = await connection.findAll();
+    const resultSave = await connection.save();
+    console.log(resultSave);
+    //const resultFindById = await connection.findById();
+    //console.log(resultFindById);
+    //const resultDelete = await connection.deleteById();
+    //console.log(resultDelete);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    try {
+      await connection.close().then(() => console.log("Connection closed"));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+main();
