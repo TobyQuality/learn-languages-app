46e484ae8b257dc202b5c44673d3e126a15a51e7
Toby Quality <topias.laatu@gmail.com>
2023-12-29 06:39:11 +0200

    FinnishWords.jsx: Create a component for fetching and showing Finnish dictionary words.

---
+++ frontend/src/components/FinnishWords.jsx
@@ -0,0 +1,34 @@
+// This file contains the FinnishWords component which is used to display the Finnish words
+// that the user has added to their list of words to learn.
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const WordList = () => {
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    // Fetch words from the backend when the component mounts
+    axios
+      .get("http://localhost:8080/api/languages/finnish")
+      .then((response) => {
+        setWords(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching words:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Finnish Words</h2>
+      <ul>
+        {words.map((word) => (
+          <li key={word.id}>{word.word}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default WordList;
